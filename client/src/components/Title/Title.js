5a4133da076eeb8a79bae9f03cc85ec8a8b5f874
user <gibson.stephen92@gmail.com>
2019-06-08 23:21:53 +0100

    simple front end started

---
+++ client/src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Title = props => {
+  return (
+    <div>
+      <h2>
+        This API will allow you to get data about sources of chocolates, be it
+        independent or supermarket, that source their chocolate ethically
+      </h2>
+    </div>
+  );
+};
+
+export default Title;

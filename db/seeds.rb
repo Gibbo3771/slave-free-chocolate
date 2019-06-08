afc7de7d3b507d5eab7bcbcd32c1d7984daa2163
user <gibson.stephen92@gmail.com>
2019-06-08 15:46:27 +0100

    added more sources

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,7 @@ utz_stamp = Stamp.create(title:"UTZ Certified", description:"The Rainforest Alli
 
 home_grown_stamp = Stamp.create(title:"In-House Grown Cocao")
 local_grown_stamp = Stamp.create(title:"Locally Grown Cocao")
+askinosie_stamp = Stamp.create(title:"Direct Trade", href:"https://askinosie.com/learn/direct-trade.html")
 
 
 aldi = Source.create(name:"Aldi", href:"https://www.aldi.co.uk/")
@@ -40,15 +41,25 @@ aloha_feels = Source.create(name:"Aloha Feels", href:"https://alohafeels.com/")
 alma_chocolate = Source.create(name:"Alma Chocolate", href:"http://www.almachocolate.com/")
 altereco = Source.create(name:"Alter Eco Foods", href:"https://www.alterecofoods.com/collections/chocolate-bars")
 amano = Source.create(name:"Amano Artisan Chocolate", href:"http://amanochocolate.com")
+askinosie = Source.create(name:"Askinosie Chocolate", href:"https://askinosie.com/")
+beach_chocolate_factory = Source.create(name:"Beach Chocolate Factory", href:"https://beachchocolatefactory.com/")
+belicious = Source.create(name:"Belicious", href:"https://belicious.ca/")
+black_mountain_chocolate = Source.create(name:"Black Mountain Chocolate", href:"https://www.blackmountainchocolate.com/")
+aacaoteca = Source.create(name:"Cacaoteca", href:"https://cacaoteca.com/")
+caribean_chocolate = Source.create(name:"Caribeans", href:"http://www.caribeanschocolate.com/")
+castronovo_chocolate = Source.create(name:"Castronovo Chocolate", href:"https://www.castronovochocolate.com/")
+charm_school_chocolate = Source.create(name:"Charm School Chocolate", href:"https://www.charmschoolchocolate.com/")
+chocolate_and_love = Source.create(name:"Chocolate and Love", href:"https://chocolateandlove.com/")
+chocolate_cartel = Source.create(name:"Chocolate Cartel", href:"https://chocolatecartel.com/")
+chocolatceleste = Source.create(name:"Chocolat Celeste", href:"https://chocolatceleste.com/")
+chocolate_tree = Source.create(name:"Chocolate Tree", href:"https://www.choctree.co.uk/")
 
-aldi.stamps << utz_stamp
-aldi.stamps << fairtrade_stamp
-aldi.stamps << rainforest_stamp
-
+aldi.stamps << [utz_stamp, fairtrade_stamp, rainforest_stamp]
 aloha_feels.stamps << home_grown_stamp
-
-alma_chocolate << home_grown_stamp
-
-altereco << fairtrade_stamp
-
-amano << local_grown_stamp
+alma_chocolate.stamps << home_grown_stamp
+altereco.stamps << fairtrade_stamp
+amano.stamps << local_grown_stamp
+askinosie.stamps << askinosie_stamp
+beach_chocolate_factory.stamps << home_grown_stamp
+belicious.stamps << rainforest_stamp
+chocolate_and_love.stamps << fairtrade_stamp
